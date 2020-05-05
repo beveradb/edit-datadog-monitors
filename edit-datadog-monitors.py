180c5bc0cec33545a8942b65c0f2b372bc5cbef1
Andrew Beveridge <andrew.beveridge@imaginecurve.com>
2020-05-05 18:06:52 +0100

    Initial debugging

---
+++ edit-datadog-monitors.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+#
+# Save existing monitors to a file and allow them to be edited in place, before uploading them again.
+
+import json
+import os
+from datadog import initialize
+from dotenv import load_dotenv
+
+load_dotenv()
+
+options = {
+    'api_key': os.getenv("DD_API_KEY"),
+    'app_key': os.getenv("DD_APP_KEY")
+}
+
+initialize(**options)
+
+from datadog import api
+
+datadog_monitors = api.Monitor.get_all()
+
+print(json.dumps(datadog_monitors, indent=2, sort_keys=False))
+
+
