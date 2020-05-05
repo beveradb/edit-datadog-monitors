19abed888df7e512f73d434aa02d516e4ad3df13
Andrew Beveridge <andrew.beveridge@imaginecurve.com>
2020-05-05 18:20:53 +0100

    Finally working!

--- edit-datadog-monitors.py
+++ edit-datadog-monitors.py
@@ -21,6 +21,20 @@ from datadog import api
 
 datadog_monitors = api.Monitor.get_all()
 
-print(json.dumps(datadog_monitors, indent=2, sort_keys=False))
+# print(json.dumps(datadog_monitors, indent=2, sort_keys=False))
 
+with open('existing_monitors.json', 'w') as outfile:
+    json.dump(datadog_monitors, outfile, indent=2, sort_keys=False)
 
+while True:
+    wait = input("Edit 'existing_monitors.json', save, then press a key to upload the changes.")
+    with open('existing_monitors.json', 'r') as infile:
+        try:
+            imported_datadog_monitors = json.load(infile)
+            break
+        except ValueError as err:
+            print("Check your json and try again!", err)
+
+for monitor in imported_datadog_monitors:
+    print("Uploading " + monitor['name'])
+    response = api.Monitor.update(**monitor)
